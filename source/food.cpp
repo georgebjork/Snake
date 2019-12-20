9515223b06465b63c138ca0820afaa17eed3fd94
georgebjork <georgebjork@outlook.com>
2019-12-20 00:35:35 -0800

    made the x coordinates only be a random even number because of the way the snake moves

--- source/food.cpp
+++ source/food.cpp
@@ -1,16 +1,22 @@
 #include "Food.h"
 
-Food::Food(COORD c) : GameObject("F")
+Food::Food() : GameObject("F")
 {
-    coord = c;
     setCoord();
 }
 
 //Set coord for food
 void Food::setCoord()
 {
-    //Set random X
-    coord.X = rand() % (MAX_X - 1) + 1;
+    //Set random X thats an even number
+    while(true)
+    {
+        coord.X = rand() % (MAX_X - 1) + 1;
+        //Check to see if its an even number
+        if(coord.X % 2 == 0){break;}
+        else{}
+    }
+    
     //Set random Y
     coord.Y = rand () % (MAX_Y - 1) + 1;
 }
