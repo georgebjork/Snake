7516fe3dc8e09da49a41195beb36d7811179e8e1
georgebjork <georgebjork@outlook.com>
2019-12-20 00:36:16 -0800

    added a check to see if the snake if the snake piece is a head or not

--- source/snake.cpp
+++ source/snake.cpp
@@ -3,6 +3,16 @@
 Snake::Snake(COORD c) : GameObject("*")
 {
     coord = c;
+    //The head will always spawn at (15,15) therefore, it must be the head
+    if(c.X == 15 && c.Y == 15)
+    {
+        is_head = true;
+    }
+    else
+    {
+        is_head = false;
+    }
+   
 }
 
 //This will return the shape of the object which is needed to print out objects
